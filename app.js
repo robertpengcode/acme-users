9505e3a2753cebaf9a8272fd96d543fbc11b3ff7
RobertPeng <jenpopeng@gmail.com>
2020-01-29 14:20:13 -0500

    add hashchange

--- app.js
+++ app.js
@@ -1,69 +1,38 @@
-
 const userList = document.querySelector('#userList')
-//const lastaNames = document.querySelector('#lastNames')
-
 const numberList = document.querySelector('#numberList')
 
-
-
 const renderFirstNames = (users) => {
-    const userObj = users.users;
-    const html = userObj.map( (user) => {
+    const html = users.map( (user) => {
         return `<tr><td>${user.firstName}</td>
         <td>${user.lastName}</td>
         <td>${user.email}</td>
         <td>${user.title}</td></tr>`;
-
     }).join('');
-
-    console.log(html);
-
+    
     userList.innerHTML = html;
 }
 
-// const renderLastNames = (users) => {
-//     const userObj = users.users;
-//     const html = userObj.map( (user) => {
-//         return `<tr>${user.lastName}</tr>`;
-
-//     }).join('');
-
-//     lastNames.innerHTML = html;
-// }
-
-const renderNumbers = () => {
-
+const renderNumbers = (count) => {
     let html = '';
-
-    for (let i = 0; i <= 23; i++) {
-
-        html +=
-        
-        `
-        <li>${i+1}
-            <a href='acme-users-api-rev.herokuapp.com/api/users/${i}'
-        </li>
-        
-        `
-        
-
+    for (let i = 1; i <= (count/50); i++) {
+        html +=       
+        `<li>${i}<a href='#${i}'></li>`;
     }
     numberList.innerHTML = html;
 }
 
-renderNumbers();
-
-
-
-
 fetch('https://acme-users-api-rev.herokuapp.com/api/users')
     .then(response => response.json())
-    //.then(response => console.log(response))
-    //.then(result => console.log(result));
-    .then(result => renderFirstNames(result))
+    .then(result => {renderNumbers(result.count);
+        renderFirstNames(result.users);
+    })
+    
+window.addEventListener('hashchange', (ev) => {
+    const id = window.location.hash.slice(1);
+    //console.log(id);
+    fetch(`https://acme-users-api-rev.herokuapp.com/api/users/${id}`)
+        .then(response => response.json())
+        .then(result => renderFirstNames(result.users))
+})
 
-// fetch('https://acme-users-api-rev.herokuapp.com/api/users')
-//     .then(response => response.json())
-//     //.then(response => console.log(response))
-//     //.then(result => console.log(result));
-//     .then(result => renderLastNames(result))
+    
